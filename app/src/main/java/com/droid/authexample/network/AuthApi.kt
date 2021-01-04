69a635ca2166abe2e75a311d13621bb4814404cf
PeteHack <pwachira900@gmail.com>
2021-01-04 11:23:42 +0300

    add interface with login method

--- app/src/main/java/com/droid/authexample/network/AuthApi.kt
+++ app/src/main/java/com/droid/authexample/network/AuthApi.kt
@@ -1,5 +1,6 @@
 package com.droid.authexample.network
 
+import com.droid.authexample.responses.LoginResponse
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -7,8 +8,8 @@ import retrofit2.http.POST
 interface AuthApi {
     @FormUrlEncoded
     @POST("auth/login")
-    fun login(
+    suspend fun login(
         @Field("email") email :String,
         @Field("password") password :String
-    ): Any
+    ): LoginResponse
 }
