04e0e0e6a401d3ee37f955d7a98a5f7ade995840
PeteHack <pwachira900@gmail.com>
2021-01-04 14:31:33 +0300

    create view model and view model factory instance

--- app/src/main/java/com/droid/authexample/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/droid/authexample/ui/auth/LoginFragment.kt
@@ -1,20 +1,34 @@
 package com.droid.authexample.ui.auth
 
+import android.os.BaseBundle
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.droid.authexample.R
+import com.droid.authexample.databinding.FragmentLoginBinding
+import com.droid.authexample.network.AuthApi
+import com.droid.authexample.repository.AuthRepository
+import com.droid.authexample.ui.auth.base.BaseFragment
 
 
-class LoginFragment : Fragment() {
+class LoginFragment : BaseFragment<AuthViewModel,FragmentLoginBinding,AuthRepository>() {
+    override fun getViewModel() = AuthViewModel::class.java
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
     }
-    }
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentLoginBinding.inflate(inflater,container,false)
+
+    override fun geFragmentrepository() = AuthRepository(remoteDataSource.buildApi(AuthApi::class.java))
+
+
+
+}
 
--- app/src/main/java/com/droid/authexample/ui/auth/base/BaseFragment.kt
+++ app/src/main/java/com/droid/authexample/ui/auth/base/BaseFragment.kt
@@ -6,17 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
+import com.droid.authexample.network.RemoteDataSource
 import com.droid.authexample.repository.BaseRepository
 
 abstract class BaseFragment<VM: ViewModel, B: ViewBinding,R: BaseRepository> : Fragment(){
     protected  lateinit var  binding: B
+    protected val remoteDataSource = RemoteDataSource()
+    protected  lateinit var  viewModel: VM
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = getFragmentBinding(inflater,container)
+        val factory = ViewModelFactory(geFragmentrepository())
+        viewModel = ViewModelProvider(this,factory).get(getViewModel())
         return binding.root
     }
 
@@ -25,4 +33,6 @@ abstract class BaseFragment<VM: ViewModel, B: ViewBinding,R: BaseRepository> : F
     abstract  fun getFragmentBinding(inflater: LayoutInflater,container: ViewGroup?) :B
 
     abstract  fun geFragmentrepository(): R
+
+
 }
