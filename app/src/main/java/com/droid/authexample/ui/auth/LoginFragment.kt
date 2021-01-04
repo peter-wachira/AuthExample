5f2e87d35b8705531f2db552a9bf55448c389ab3
PeteHack <pwachira900@gmail.com>
2021-01-04 11:24:12 +0300

    create login fragment

--- app/src/main/java/com/droid/authexample/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/droid/authexample/ui/auth/LoginFragment.kt
@@ -1,10 +1,20 @@
 package com.droid.authexample.ui.auth
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.droid.authexample.R
 
 
 class LoginFragment : Fragment() {
 
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
     }
 
