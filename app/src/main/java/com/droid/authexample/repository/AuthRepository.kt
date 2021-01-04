6292f934af47e0702d075ad54ff26302f5668acc
PeteHack <pwachira900@gmail.com>
2021-01-04 11:40:26 +0300

    add auth respositor class

---
+++ app/src/main/java/com/droid/authexample/repository/AuthRepository.kt
@@ -0,0 +1,14 @@
+package com.droid.authexample.repository
+
+import com.droid.authexample.network.AuthApi
+
+class AuthRepository(
+    private val api: AuthApi
+) : BaseRepository(){
+    suspend fun login(
+        email: String,
+        password : String
+    ) = safeApiCall {
+        api.login(email,password)
+    }
+}
