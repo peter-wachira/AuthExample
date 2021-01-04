b9d23ee05938128e8811d1cd1add3814f7cdf2bb
PeteHack <pwachira900@gmail.com>
2021-01-04 14:32:34 +0300

    create constructor for auth view model

---
+++ app/src/main/java/com/droid/authexample/ui/auth/AuthViewModel.kt
@@ -0,0 +1,11 @@
+package com.droid.authexample.ui.auth
+
+import androidx.lifecycle.ViewModel
+import com.droid.authexample.repository.AuthRepository
+
+class AuthViewModel(
+    private val repository : AuthRepository
+) : ViewModel(){
+
+}
+
