360ca8920e4e3b3cdb8457e5cd25041397355168
PeteHack <pwachira900@gmail.com>
2021-01-04 11:23:07 +0300

    add sealed lass for error handling'

--- app/src/main/java/com/droid/authexample/network/Resource.kt
+++ app/src/main/java/com/droid/authexample/network/Resource.kt
@@ -5,8 +5,8 @@ import okhttp3.ResponseBody
 sealed class Resource<out T> {
     data class Success<out T>(val value :T): Resource<T>()
     data class  Failure(
-        val isNetworkError: Boolean,
-        val errorCode: Int,
-        val errorBody: ResponseBody?
-    )
+            val isNetworkError: Boolean,
+            val errorCode: Int?,
+            val errorBody: ResponseBody?
+    ): Resource<Nothing>()
 }
