61324accac374e61a2d510bb670df5fd734bb5a3
PeteHack <pwachira900@gmail.com>
2021-01-05 10:06:21 +0300

    create login reponse observer for success and failure

--- app/src/main/java/com/droid/authexample/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/droid/authexample/ui/auth/LoginFragment.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.droid.authexample.R
 import com.droid.authexample.databinding.FragmentLoginBinding
 import com.droid.authexample.network.AuthApi
+import com.droid.authexample.network.Resource
 import com.droid.authexample.repository.AuthRepository
 import com.droid.authexample.ui.auth.base.BaseFragment
+import java.util.*
 
 
 class LoginFragment : BaseFragment<AuthViewModel,FragmentLoginBinding,AuthRepository>() {
@@ -18,6 +21,28 @@ class LoginFragment : BaseFragment<AuthViewModel,FragmentLoginBinding,AuthReposi
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        viewModel.loginResponse.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            when(it){
+                is Resource.Success->{
+                    Toast.makeText(requireContext(), it.toString(),Toast.LENGTH_LONG).show()
+                }
+                is Resource.Failure ->{
+                    Toast.makeText(requireContext(), "Login Failure",Toast.LENGTH_LONG).show()
+                }
+            }
+
+        })
+
+        binding.buttonLogin.setOnClickListener {
+            val email = binding.editTextTextEmailAddress.text.toString().trim()
+            val password = binding.editTextTextPassword.text.toString().trim()
+
+            //@todo add validations
+            viewModel.login(email, password)
+
+        }
+
 
     }
 
