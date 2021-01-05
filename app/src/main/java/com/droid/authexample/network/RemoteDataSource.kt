0b3eadfa9ddcbdfca6eb30e2b3661e6380f82907
PeteHack <pwachira900@gmail.com>
2021-01-05 12:11:32 +0300

    add login interceptor implementation

--- app/src/main/java/com/droid/authexample/network/RemoteDataSource.kt
+++ app/src/main/java/com/droid/authexample/network/RemoteDataSource.kt
@@ -1,5 +1,8 @@
 package com.droid.authexample.network
 
+import com.droid.authexample.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -14,8 +17,22 @@ class RemoteDataSource {
     ): Api {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor { chain ->
+                        chain.proceed(chain.request().newBuilder().also {
+                            it.addHeader("Authorization", "Bearer $authToken")
+                        }.build())
+                    }.also { client ->
+                        if (BuildConfig.DEBUG) {
+                            val logging = HttpLoggingInterceptor()
+                            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+                            client.addInterceptor(logging)
+                        }
+                    }.build()
+            )
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(api)
-    }
+            .create(api)}
+
 }
