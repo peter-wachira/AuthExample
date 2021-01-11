0cb4b50c254f669f4cc9e26eb218d5778e4c8fe8
PeteHack <pwachira900@gmail.com>
2021-01-11 13:18:37 +0300

    add method to save aith token in auth repository

--- app/src/main/java/com/droid/authexample/data/repository/AuthRepository.kt
+++ app/src/main/java/com/droid/authexample/data/repository/AuthRepository.kt
@@ -1,14 +1,19 @@
 package com.droid.authexample.data.repository
-
+import com.droid.authexample.data.UserPreferences
 import com.droid.authexample.data.network.AuthApi
 
 class AuthRepository(
-    private val api: AuthApi
+    private val api: AuthApi,
+    private val preferences: UserPreferences
 ) : BaseRepository(){
     suspend fun login(
         email: String,
         password : String
     ) = safeApiCall {
         api.login(email,password)
+    }
+
+    suspend fun  saveAuthToken( token: String){
+      preferences.saveAuthToken(token)
     }
  }
