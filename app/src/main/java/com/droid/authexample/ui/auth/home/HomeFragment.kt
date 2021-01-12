8ef77dee00c672f3a2240d5f58356c6ba299d93c
PeteHack <pwachira900@gmail.com>
2021-01-12 10:32:15 +0300

    modify getFragment repository method

--- app/src/main/java/com/droid/authexample/ui/auth/home/HomeFragment.kt
+++ app/src/main/java/com/droid/authexample/ui/auth/home/HomeFragment.kt
@@ -5,22 +5,64 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.droid.authexample.R
+import androidx.lifecycle.Observer
+import com.droid.authexample.data.network.Resource
+import com.droid.authexample.data.network.UserApi
+import com.droid.authexample.data.repository.UserRepository
+import com.droid.authexample.data.responses.User
+import com.droid.authexample.databinding.FragmentHomeBinding
+import com.droid.authexample.ui.auth.base.BaseFragment
+import com.droid.authexample.ui.auth.visible
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<HomeViewModel,FragmentHomeBinding,UserRepository>() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.progressbar.visible(false)
+        viewModel.getUser()
+        viewModel.user.observe(viewLifecycleOwner, Observer {
+            when(it){
+                is Resource.Success ->{
+                    binding.progressbar.visible(false)
+                    updateUI(it.value.user)
+                }
+                is Resource.Loading ->{
+                    binding.progressbar.visible(true)
+                }
+            }
+        })
     }
 
+    private fun updateUI(user :User ) {
+        with(binding){
+            textViewEmail.text = user.email
+            textViewName.text =user.name
+            textViewId.text =user.id.toString()
+        }
+    }
+
+
+
+    override fun getViewModel() = HomeViewModel::class.java
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    )= FragmentHomeBinding.inflate(inflater,container,false)
+
+
+    override fun geFragmentrepository() :UserRepository{
+        val token = runBlocking { userPreferences.authToken.first() }
+        val api = remoteDataSource.buildApi(UserApi::class.java)
+        return  UserRepository(api)
+    }
 
 }
