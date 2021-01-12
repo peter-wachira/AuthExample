4b341691e03c156c40e1ee97da9b2696e1b3e361
PeteHack <pwachira900@gmail.com>
2021-01-12 13:18:53 +0300

    create base view model that provide logout method to other view models

--- app/src/main/java/com/droid/authexample/ui/auth/base/BaseViewModel.kt
+++ app/src/main/java/com/droid/authexample/ui/auth/base/BaseViewModel.kt
@@ -1,2 +1,11 @@
 package com.droid.authexample.ui.auth.base
 
+import androidx.lifecycle.ViewModel
+import com.droid.authexample.data.network.UserApi
+import com.droid.authexample.data.repository.BaseRepository
+
+abstract class BaseViewModel(
+    private  val repository: BaseRepository
+) : ViewModel() {
+    suspend fun  logout(api:UserApi)= repository.logout(api)
+}
