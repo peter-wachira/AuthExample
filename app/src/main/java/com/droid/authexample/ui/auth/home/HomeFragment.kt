52f852a89fe2d85686c49978fcdb6f668f019974
PeteHack <pwachira900@gmail.com>
2021-01-12 12:20:36 +0300

    override methods from base fragment

--- app/src/main/java/com/droid/authexample/ui/auth/home/HomeFragment.kt
+++ app/src/main/java/com/droid/authexample/ui/auth/home/HomeFragment.kt
@@ -18,37 +18,38 @@ import kotlinx.coroutines.runBlocking
 
 
 class HomeFragment : BaseFragment<HomeViewModel,FragmentHomeBinding,UserRepository>() {
-    
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.progressbar.visible(false)
+
         viewModel.getUser()
+
         viewModel.user.observe(viewLifecycleOwner, Observer {
-            when(it){
-                is Resource.Success ->{
+            when (it) {
+                is Resource.Success -> {
                     binding.progressbar.visible(false)
                     updateUI(it.value.user)
                 }
-                is Resource.Loading ->{
+                is Resource.Loading -> {
                     binding.progressbar.visible(true)
                 }
             }
         })
-    }
 
-    private fun updateUI(user :User ) {
-        with(binding){
-            textViewEmail.text = user.email
-            textViewName.text =user.name
-            textViewId.text =user.id.toString()
+        binding.buttonLogout.setOnClickListener {
+//            logout()
         }
     }
 
+    private fun updateUI(user: User) {
+        with(binding) {
+            textViewId.text = user.id.toString()
+            textViewName.text = user.name
+            textViewEmail.text = user.email
+        }
+    }
 
 
     override fun getViewModel() = HomeViewModel::class.java
@@ -56,13 +57,12 @@ class HomeFragment : BaseFragment<HomeViewModel,FragmentHomeBinding,UserReposito
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    )= FragmentHomeBinding.inflate(inflater,container,false)
+    ) = FragmentHomeBinding.inflate(inflater, container, false)
 
 
-    override fun geFragmentrepository() :UserRepository{
+    override fun geFragmentrepository(): UserRepository {
         val token = runBlocking { userPreferences.authToken.first() }
-        val api = remoteDataSource.buildApi(UserApi::class.java)
-        return  UserRepository(api)
+        val api = remoteDataSource.buildApi(UserApi::class.java, token)
+        return UserRepository(api)
     }
-
 }
