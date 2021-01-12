f2b24bcfe70a279d13cb971fac2e61b13e14f8e8
PeteHack <pwachira900@gmail.com>
2021-01-12 12:53:15 +0300

    add method to handle api error

--- app/src/main/java/com/droid/authexample/ui/auth/Utils.kt
+++ app/src/main/java/com/droid/authexample/ui/auth/Utils.kt
@@ -2,6 +2,10 @@ package com.droid.authexample.ui.auth
 import android.app.Activity
 import android.content.Intent
 import android.view.View
+import androidx.fragment.app.Fragment
+import com.droid.authexample.data.network.Resource
+import com.droid.authexample.ui.auth.auth.LoginFragment
+import com.google.android.material.snackbar.Snackbar
 
 fun<A: Activity> Activity.startNewActivity(activity: Class<A>){
     Intent(this,activity).also {
@@ -17,4 +21,38 @@ fun View.visible(isVisible: Boolean){
 fun View.enable(enabled: Boolean){
     isEnabled = enabled
     alpha = if (enabled) 1f else 0.5f
+}
+
+
+fun View.snackbar(message: String, action: (() -> Unit)? = null) {
+    val snackbar = Snackbar.make(this, message, Snackbar.LENGTH_LONG)
+    action?.let {
+        snackbar.setAction("Retry") {
+            it()
+        }
+    }
+    snackbar.show()
+}
+
+fun Fragment.handleApiError(
+    failure: Resource.Failure,
+    retry: (() -> Unit)? = null
+) {
+    when {
+        failure.isNetworkError -> requireView().snackbar(
+            "Please check your internet connection",
+            retry
+        )
+        failure.errorCode == 401 -> {
+            if (this is LoginFragment) {
+                requireView().snackbar("You've entered incorrect email or password")
+            } else {
+//                (this as BaseFragment<*, *, *>).logout()
+            }
+        }
+        else -> {
+            val error = failure.errorBody?.string().toString()
+            requireView().snackbar(error)
+        }
+    }
 }
