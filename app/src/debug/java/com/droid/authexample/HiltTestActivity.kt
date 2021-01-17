02eeb00fb28b2d65fb839821c9a47c8c31ea0054
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-18 00:28:06 +0300

    add a fake test activity to host fragment testing

---
+++ app/src/debug/java/com/droid/authexample/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package com.droid.authexample
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
