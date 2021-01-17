9695164bce33ea105778976022a0ee4737115c8a
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-17 20:49:29 +0300

    delete data source responsible for building the api as it is no longer needed,hilt simplifies its task through injection

--- app/src/main/java/com/droid/authexample/ui/auth/base/BaseFragment.kt
+++ app/src/main/java/com/droid/authexample/ui/auth/base/BaseFragment.kt
@@ -5,23 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
 import com.droid.authexample.data.UserPreferences
-import com.droid.authexample.data.network.RemoteDataSource
-import com.droid.authexample.data.network.UserApi
 import com.droid.authexample.data.repository.BaseRepository
-import com.droid.authexample.ui.auth.auth.AuthActivity
-import com.droid.authexample.ui.auth.startNewActivity
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 abstract class BaseFragment<B: ViewBinding,R: BaseRepository> : Fragment(){
     protected  lateinit var  binding: B
-    protected val remoteDataSource = RemoteDataSource()
 
     @Inject
     lateinit var userPreferences: UserPreferences
