76c6351b34c86be3081ecc073700373175e225de
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-18 11:07:51 +0300

    test all general edge cases for email validator

--- app/src/test/java/com/droid/authexample/utils/EmailValidatorTest.kt
+++ app/src/test/java/com/droid/authexample/utils/EmailValidatorTest.kt
@@ -1,5 +1,6 @@
 package com.droid.authexample.utils
 
+import junit.framework.Assert.assertFalse
 import junit.framework.Assert.assertTrue
 import junit.framework.TestCase
 import org.junit.Test
@@ -11,5 +12,25 @@ class EmailValidatorTest {
     @Test
     fun `Correct Email Returns True`(){
         assertTrue(EmailValidator.isValidEmail("name@gmail.com"))
+    }
+    @Test
+    fun `Invalid Email Returns False`(){
+        assertFalse(EmailValidator.isValidEmail("name"))
+    }
+    @Test
+    fun `Correct Email Subdomain Returns True` (){
+        assertTrue(EmailValidator.isValidEmail("name@protoenergy.com"))
+    }
+    @Test
+    fun `Invalid Double Dot Returns False` (){
+        assertFalse(EmailValidator.isValidEmail("name@protoenergy..com"))
+    }
+    @Test
+    fun `Invalid Email No Username Returns False` (){
+        assertFalse(EmailValidator.isValidEmail("@protoenergy.com"))
+    }
+    @Test
+    fun `Null Email Returns False` (){
+        assertFalse(EmailValidator.isValidEmail(""))
     }
 }
