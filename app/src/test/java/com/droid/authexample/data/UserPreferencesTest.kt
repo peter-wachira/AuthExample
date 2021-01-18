85db58e8b27aa306cb8c5933b1417054d61433b8
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-18 11:14:28 +0300

    add user preference test

---
+++ app/src/test/java/com/droid/authexample/data/UserPreferencesTest.kt
@@ -0,0 +1,13 @@
+package com.droid.authexample.data
+
+import junit.framework.Assert.assertEquals
+import junit.framework.TestCase
+import org.junit.Test
+
+class UserPreferencesTest{
+
+    @Test
+    fun `happy Test case` (){
+        assertEquals(2,1+1)
+    }
+}
